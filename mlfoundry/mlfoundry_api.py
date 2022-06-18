9104d9f8dd5e3ebf39e905c618056cf5e39163fc
unknown <sanwal@truefoundry.com>
2022-06-18 13:30:59 +0530

    Added script run part

--- mlfoundry/mlfoundry_api.py
+++ mlfoundry/mlfoundry_api.py
@@ -1,13 +1,9 @@
-import math
-print(math.sqrt(0))
 """This is my example docstring
 
-    A simple bullet list:
+A simple bullet list:
 
     - point 1
     - point 2
     - point 3
 
 """
-print(math.sqrt(4))
-print(math.sqrt(3.5))
