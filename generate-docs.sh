ce1c53827ced1216b98b36e61a6e37cd4c1e8a5e
unknown <sanwal@truefoundry.com>
2022-06-18 13:13:10 +0530

    Added script run part

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,10 +1,10 @@
 lazydocs \
     --output-path="./docs/" \
-    --src-base-url="https://github.com/sanwal-truefoundry/Creating-Storing-PY-Files/tree/script-run/" \
+    --src-base-url="https://github.com/sanwal-truefoundry/Creating-Storing-PY-Files/tree/script-run" \
     ./mlfoundry/mlfoundry_run.py
 
 
 lazydocs \
     --output-path="./docs/" \
-    --src-base-url="https://github.com/sanwal-truefoundry/Creating-Storing-PY-Files/tree/script-run/" \
+    --src-base-url="https://github.com/sanwal-truefoundry/Creating-Storing-PY-Files/tree/script-run" \
     ./mlfoundry/mlfoundry_api.py
