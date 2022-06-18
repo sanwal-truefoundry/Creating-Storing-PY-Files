35b3a407497106ef341000fb8bdaba808f8c54e3
unknown <sanwal@truefoundry.com>
2022-06-18 14:30:29 +0530

    Added script run part

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,5 +1,5 @@
 lazydocs \
-    --output-path="./docs/api-docs/mlfoundry_api.md" \
+    --output-path="./docs/api-docs/" \
     --overview-file="README.md" \
     --src-base-url="https://github.com/sanwal-truefoundry/Creating-Storing-PY-Files/tree/main/" \
     ./mlfoundry/mlfoundry_api.py
