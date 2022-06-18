8c2c99df18a660dab621139ab43cca0d3f7b62f4
unknown <sanwal@truefoundry.com>
2022-06-18 19:01:38 +0530

    Added script run part

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,2 +1,2 @@
 lazydocs --output-path stdout ./mlfoundry/mlfoundry_api.py
-lazydocs --output-path ./docs/api-doc/mlfoundry_api.py.md ./mlfoundry/mlfoundry_api.py
+lazydocs --output-path sanwal-truefoundry/Creating-Storing-PY-Files/tree/script-run/docs/api-docs ./mlfoundry/mlfoundry_api.py
