c097e2c4e67237cb8d5af82468b1a5b19587b5bd
unknown <sanwal@truefoundry.com>
2022-06-18 13:11:45 +0530

    Added script run part

--- mlfoundry/mlfoundry_run.py
+++ mlfoundry/mlfoundry_run.py
@@ -2,6 +2,15 @@ import math
 def check(n):
 	if n == 1:
 		return False
+    """This is my example docstring
+
+        A simple bullet list:
+
+        - point 1
+        - point 2
+        - point 3
+
+    """
 	for x in range(2, (int)(math.sqrt(n))+1):
 		if n % x == 0:
 			return False
