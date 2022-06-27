8fdc01c3157f532f3617b2a0c3dd9082f546cfd9
unknown <sanwal@truefoundry.com>
2022-06-27 15:45:16 +0530

    Added collaboration

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,3 +1,3 @@
-lazydocs --output-path ./docs/api/ ./mlfoundry/mlfoundry_api.py
-lazydocs --output-path ./docs/api/ ./mlfoundry/mlfoundry_run.py
-lazydocs --output-path ./docs/api/ ./mlfoundry/mlfoundry_try.py
+lazydocs --output-path ./tmp_ahc/docs/api/ ./mlfoundry/mlfoundry_api.py
+lazydocs --output-path ./tmp_ahc/docs/api/ ./mlfoundry/mlfoundry_run.py
+lazydocs --output-path ./tmp_ahc/docs/api/ ./mlfoundry/mlfoundry_try.py
