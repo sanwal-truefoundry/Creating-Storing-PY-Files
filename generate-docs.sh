a621c26e12f7e8cf51ad52140f9fd13f0245cc49
unknown <sanwal@truefoundry.com>
2022-06-28 16:54:51 +0530

    Added collaboration

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,4 +1,4 @@
 lazydocs --output-path ./docs/experiment-tracking/api-reference/ --no-watermark \
     ./mlfoundry/mlfoundry_run.py \
     ./mlfoundry/mlfoundry_api.py \
-    ./mlfoundry/mlfoundry_try.py
+    ./mlfoundry/mlfoundry_try.py    
